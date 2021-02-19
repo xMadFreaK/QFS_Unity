6126094d1cbd342f801f1ee685ce4642b21c95d0
Linda H <lindah0503@gmail.com>
2021-02-19 15:24:16 +0100

    load Methode

--- QFS_Project/Assets/Scripts/Utility/GameUtility.cs
+++ QFS_Project/Assets/Scripts/Utility/GameUtility.cs
@@ -71,4 +71,34 @@ public class Data
             return data;
         }
     }
+
+    public static Data Load(/*Übergabe des Files*/)
+    {
+        /* wenn Zielfile (xml Dokument) noch nicht existiert, dann soll es erzeugt werden.
+         *  if (!File.Exists(GameUtility.xmlFilePath)) { result = false; return new Data(); }
+        XmlSerializer deserializer = new XmlSerializer(typeof(Data));
+        using(Stream stream = new FileStream(GameUtility.xmlFilePath, FileMode.Open))
+        {
+            var data = (Data)deserializer.Deserialize(stream);
+
+            result = true;
+            return data;
+        }
+         * 
+         * 
+         * wenn in dem xml Dokument etwas existiert, wird es rausgelöscht
+         * 
+         * Fetchen der Daten des Übergabedokuments
+         * 
+         * Übergabe an das Zielfile (write)
+         * 
+         */
+
+    }
+
+
+    /*public void UploadData() ---> lädt Files in eine Datenbank (und übersetzt csv in xml?)
+     * 
+     * 
+     */
 }
